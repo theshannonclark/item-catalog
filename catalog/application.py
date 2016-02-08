eef2bd657f9cd9bfa2e7e35af0523404c914bbb8
Shannon Clark <theshannonclark@gmail.com>
2016-02-08 15:14:02 +1100

    Add database connection code to the application file
    Added some preliminary code to the application.py for connecting
    to the database.

--- catalog/application.py
+++ catalog/application.py
@@ -1,6 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Item
+
+engine = create_engine('postgresql:///catalog')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
 @app.route('/')
 def home():
     return "Item Catalog"
