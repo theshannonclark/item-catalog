2ca6e4351b33dbee0cc9ad4b58390f4665d223cb
Shannon Clark <theshannonclark@gmail.com>
2016-02-15 14:04:34 +1100

    Fix foreign key issues
    I didn't realise that I needed to create the foreign key separately
    from adding the relationship.

--- catalog/database_setup.py
+++ catalog/database_setup.py
@@ -2,8 +2,8 @@ import sys
 
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -13,13 +13,17 @@ class Category(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+    items = relationship("Item", order_by="Item.id", back_populates="category")
+
 class Item(Base):
     __tablename__ = 'item'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
     description = Column(String(250))
-    category_id = relationship(Category)
+    category_id = Column(Integer, ForeignKey('category.id'))
+
+    category = relationship("Category", back_populates="items")
 
 engine = create_engine('postgresql:///catalog')
 Base.metadata.create_all(engine)
