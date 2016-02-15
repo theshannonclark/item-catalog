d54edb6383e74af4a0a1ca03d08c09cbfaa1b52e
Shannon Clark <theshannonclark@gmail.com>
2016-02-15 16:06:10 +1100

    Convert index.html into a template file
    Added some temporary data in dictionaries, and modified index.html
    to display it.

--- catalog/application.py
+++ catalog/application.py
@@ -1,19 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Category, Item
+#from sqlalchemy import create_engine
+#from sqlalchemy.orm import sessionmaker
+#from database_setup import Base, Category, Item
 
-engine = create_engine('postgresql:///catalog')
-Base.metadata.bind = engine
+#engine = create_engine('postgresql:///catalog')
+#Base.metadata.bind = engine
 
-DBSession = sessionmaker(bind = engine)
-session = DBSession()
+#DBSession = sessionmaker(bind = engine)
+#session = DBSession()
+
+categories = [
+    {'name': 'Arts & Photography'},
+    {'name': 'Fantasy'},
+    {'name': 'Science Fiction'}
+]
+
+books = [
+    {'name': 'The Name of the Wind'},
+    {'name': 'The Lies of Locke...'},
+    {'name': 'Perdido Street Station'},
+    {'name': 'Gardens of the Moon'},
+    {'name': 'The Way of Kings'}
+]
 
 @app.route('/')
 def home():
-    return "Item Catalog"
+    return render_template("index.html", categories=categories, books=books)
 
 if __name__ == '__main__':
     app.debug = True
